4245441e01ce53cc634a47869733986f65b9efdb
hwangiris <42462259+hwangiris@users.noreply.github.com>
2022-04-22 12:05:52 +0800

    quiz + select

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,6 +6,13 @@ var isMac = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
 var mobileSafari = (/iPhone/i.test(navigator.platform) || /iPod/i.test(navigator.platform) || /iPad/i.test(navigator.userAgent)) && !!navigator.appVersion.match(/(?:Version\/)([\w\._]+)/);
 var Android = (navigator.userAgent.match(/android/i) != null);
 
+let headerOffset = 56;
+if ( width >=  992 ) {
+  headerOffset = 70;
+} else {
+  headerOffset = 56;
+}
+
 function preventDefault(e) {
 	e.preventDefault();
 }
@@ -44,163 +51,363 @@ function enableScroll() {
 	window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
 }
 
-$(function () {
-	$('body').addClass('noscroll');
-	$('.test-start').click(function () {
-		$('.op-block').slideUp(700);
-		disableScroll();
-		$('body').removeClass('noscroll');
-	});
+$('body').addClass('noscroll');
+let testOffsetTop = $('#test').offset().top;
+disableScroll();
+$('.test-start').click(function () {
+	// $('.op-block').slideUp(700);
+	resetTest();
 });
 
+if( window.location.hash === '#feature' || window.location.hash === '#hospital' ) {
+	enableScroll();
+	$('body').removeClass('noscroll');
+}
+
 //改字內容
-var quiz = [
+const quiz = [
 	{
-		"question": "1. 睡眠困難，譬如難以入睡、易睡或早醒",
-		"img": "<div class='illustration illustration-1'><img src='images/q1.png' alt=''><img src='images/q1-move.png' class='img-move' alt=''></div>",
-		"choices": ["0", "1", "2", "3", "4"],
-		"comment": "<span>仔細回想最近一週（包含今天），對於這個問題的困擾程度：</span><span>完全沒有給0分、輕微給1分、中等程度給2分、厲害給3分、非常厲害給4分</span>",
-		"gtm": ""
+		title: '下列兩種情況，你比較符合哪一種？',
+		id: 'test-q1',
+		prev: 'test-q0',
+		option: [
+			{
+				content: '突然胸痛',
+				track: 'test-q1-a',
+				next: 'test-q2',
+				result: null
+			},
+			{
+				content: '安靜時突然覺得心臟砰砰跳（運動或興奮時除外）',
+				track: 'test-q1-b',
+				next: 'test-q4',
+				result: null
+			}
+		],
 	},
 	{
-		"question": "2. 感覺緊張不安",
-		"img": "<div class='illustration illustration-2'><img src='images/q2.png' alt=''><img src='images/q2-move.png' class='img-move' alt=''></div>",
-		"choices": ["0", "1", "2", "3", "4"],
-		"comment": "",
-		"gtm": ""
+		title: '下列兩種情況，你比較符合哪一種？',
+		id: 'test-q2',
+		prev: 'test-q1',
+		option: [
+			{
+				content: '有火燒心、胃不適症狀，喝水吃胃藥就可緩解',
+				track: 'test-q2-a',
+				next: 'end',
+				result: 'result-a',
+			},
+			{
+				content: '有發燒症狀',
+				track: 'test-q2-b',
+				next: 'end',
+				result: 'result-b',
+			},
+			{
+				content: '沒有發燒，只有激烈且持續的胸痛',
+				track: 'test-q2-c',
+				next: 'test-q3',
+				result: null
+			}
+		],
 	},
 	{
-		"question": "3. 覺得容易苦惱或動怒",
-		"img": "<div class='illustration illustration-3'><img src='images/q3.png' alt=''><img src='images/q3-move.png' class='img-move' alt=''></div>",
-		"choices": ["0", "1", "2", "3", "4"],
-		"comment": "",
-		"gtm": ""
+		title: '下列 3 種情況，哪一種比較符合你：',
+		id: 'test-q3',
+		prev: 'test-q2',
+		option: [
+			{
+				content: '吸氣、吐氣會痛，痛的地方手指得出來',
+				track: 'test-q3-a',
+				next: 'end',
+				result: 'result-c',
+			},
+			{
+				content: '疼痛從胸部移到背部及腰部',
+				track: 'test-q3-b',
+				next: 'end',
+				result: 'result-d'
+			},
+			{
+				content: '疼痛部位不明確，左肩、左手腕或胃、背部也同時會痛；且腳在運動時就痛，休息就會好說間持續約 1~15 分鐘',
+				track: 'test-q3-c',
+				next: 'end',
+				result: 'result-e'
+			},
+			{
+				content: '以上皆否',
+				track: 'test-q3-d',
+				next: 'end',
+				result: 'result-f'
+			}
+		],
 	},
 	{
-		"question": "4. 感覺憂鬱、心情低落",
-		"img": "<div class='illustration illustration-4'><img src='images/q4.png' alt=''><img src='images/q4-move.png' class='img-move' alt=''></div>",
-		"choices": ["0", "1", "2", "3", "4"],
-		"comment": "",
-		"gtm": ""
+		title: '你的脈搏為以下哪種？',
+		id: 'test-q4',
+		prev: 'test-q1',
+		option: [
+			{
+				content: '＞100次／每分鐘',
+				track: 'test-q4-a',
+				next: 'test-q5',
+				result: null
+			},
+			{
+				content: '60~100次／每分鐘',
+				track: 'test-q4-b',
+				next: 'test-q6',
+				result: null
+			},
+			{
+				content: '＜60次／每分鐘',
+				track: 'test-q4-c',
+				next: 'end',
+				result: 'result-g',
+			}
+		],
 	},
 	{
-		"question": "5. 覺得比不上別人",
-		"img": "<div class='illustration illustration-5'><img src='images/q5.png' alt=''><img src='images/q5-move.png' class='img-move' alt=''></div>",
-		"choices": ["0", "1", "2", "3", "4"],
-		"comment": "",
-		"gtm": ""
+		title: '你的脈搏＞100 次／每分鐘，計算次數頻率屬於規則／不規則？',
+		id: 'test-q5',
+		prev: 'test-q4',
+		option: [
+			{
+				content: '規則',
+				track: 'test-q5-a',
+				next: 'test-q7',
+				result: null
+			},
+			{
+				content: '60~不規則',
+				track: 'test-q5-b',
+				next: 'end',
+				result: 'result-h'
+			}
+		],
 	},
 	{
-		"question": "6. 曾經有自殺的想法",
-		"img": "<div class='illustration illustration-6'><img src='images/q6.png' alt=''><img src='images/q6-move.png' class='img-move' alt=''></div>",
-		"choices": ["0", "1", "2", "3", "4"],
-		"comment": "資料來源：全國自殺防治中心「心情溫度計」",
-		"gtm": "data-category='web_web' data-action='2021mental-health' data-label='section-test-2'"
-	}
+		title: '你的脈搏 60~100 次／每分鐘，計算次數頻率屬於規則／不規則？',
+		id: 'test-q6',
+		prev: 'test-q4',
+		option: [
+			{
+				content: '規則',
+				track: 'test-q6-a',
+				next: 'test-q8',
+				result: null
+			},
+			{
+				content: '60~不規則',
+				track: 'test-q6-b',
+				next: 'end',
+				result: 'result-h'
+			}
+		],
+	},
+	{
+		title: '下列 2 種情況，哪一種較符合你？',
+		id: 'test-q7',
+		prev: 'test-q5',
+		option: [
+			{
+				content: '突然發生，慢慢緩解',
+				track: 'test-q7-a',
+				next: 'end',
+				result: 'result-i'
+			},
+			{
+				content: '突然發生瞬間緩解',
+				track: 'test-q7-b',
+				next: 'end',
+				result: 'result-j'
+			}
+		],
+	},
+	{
+		title: '下列 2 種情況，哪一種較符合你？',
+		id: 'test-q8',
+		prev: 'test-q6',
+		option: [
+			{
+				content: '兩、三下停一下',
+				track: 'test-q8-a',
+				next: 'end',
+				result: 'result-k'
+			},
+			{
+				content: '完全亂跳',
+				track: 'test-q8-b',
+				next: 'end',
+				result: 'result-l'
+			}
+		],
+	},
 ]
-var a1 = 0;
-var a2 = 0;
-var a3 = 0;
-var a4 = 0;
-var a5 = 0;
-var a6 = 0;
-var question = 0;
-var userSelect = [0, 0, 0, 0, 0, 0];
-var gameHtml = "";
 
-$(document).ready(function () {
-	for (i = 0; i < quiz.length; i++) {
-		gameHtml += "<section class='question-block' id='osq" + i + "'><div class='container'><div class='row justify-content-md-center'><div class='img-block col-9 col-md-5'>" + quiz[i]['img'] + "</div><div class='txt-block col-12 col-md-6 offset-md-1'><div class='schedule-range'><div class='schedule-line schedule-line--" + (i + 1) + "'></div></div><h3 id='q" + i + "'>" + quiz[i]['question'] + "</h3><div class='select-block'><div class='d-flex justify-content-between'><div class='h5 mx-1'>輕</div><div class='h5 mx-1'>重</div></div><div class='answer'><div class='d-flex flex-row-reverse justify-content-between rate'><input type='radio' id='star-" + i + "--5' name='rate-" + i + "' /><label " + quiz[i]['gtm'] + " for='star-" + i + "--5' class='choice star-" + i + "--5' value='4'><span>" + quiz[i]['choices'][4] + "</span></label><input type='radio' id='star-" + i + "--4' name='rate-" + i + "' /><label " + quiz[i]['gtm'] + " for='star-" + i + "--4' class='choice star-" + i + "--4' value='3'><span>" + quiz[i]['choices'][3] + "</span></label><input type='radio' id='star-" + i + "--3' name='rate-" + i + "' /><label " + quiz[i]['gtm'] + " for='star-" + i + "--3' class='choice star-" + i + "--3' value='2'><span>" + quiz[i]['choices'][2] + "</span></label><input type='radio' id='star-" + i + "--2' name='rate-" + i + "' /><label " + quiz[i]['gtm'] + " for='star-" + i + "--2' class='choice star-" + i + "--2' value='1'><span>" + quiz[i]['choices'][1] + "</span></label><input type='radio' id='star-" + i + "--1' name='rate-" + i + "' /><label " + quiz[i]['gtm'] + " for='star-" + i + "--1' class='choice star-" + i + "--1' value='0'><span>" + quiz[i]['choices'][0] + "</span></label></div></div>";
-		if ( quiz[i]['comment'] !== "" ) {
-			gameHtml += "<h5 class='comment'>" + quiz[i]['comment'] + "</h5>";
-		}
-		gameHtml += "</div></div></div></div></section>"
-	}
-	$("#gameSection").append(gameHtml);
-	$("#final").hide();
-	choiceScore();
-	function choiceScore() {
-		$(".choice").bind("click",
-			function (event) {
-				var answer = $(this).attr("value");
-				var nextQuestion = $(this).closest('.question-block').next();
-				// console.log(answer);
+const resultArray = {
+	'result-a': [{
+		name: '胃食道逆流',
+		link: 'https://kb.commonhealth.com.tw/library/40.html',
+	}],
+	'result-b': [{
+		name: '心包膜炎',
+		link: 'https://kb.commonhealth.com.tw/library/265.html',
+	},{
+		name: '心肌炎',
+		link: 'https://kb.commonhealth.com.tw/library/262.html',
+	},{
+		name: '肺炎',
+		link: 'https://kb.commonhealth.com.tw/library/36.html',
+	},{
+		name: '肌筋膜痛症候群',
+		link: 'https://kb.commonhealth.com.tw/library/122.html',
+	}],
+	'result-c': [{
+		name: '肌肉、骨骼或神經痛',
+		link: 'https://kb.commonhealth.com.tw/library/122.html',
+	}],
+	'result-d': [{
+		name: '主動脈剝離',
+		link: 'https://kb.commonhealth.com.tw/library/270.html',
+	},{
+		name: '心肌梗塞',
+		link: 'https://kb.commonhealth.com.tw/library/22.html',
+	}],
+	'result-e': [{
+		name: '心絞痛',
+		link: 'https://kb.commonhealth.com.tw/library/69.html',
+	}],
+	'result-f': [{
+		name: '心肌梗塞',
+		link: 'https://kb.commonhealth.com.tw/library/22.html',
+	}],
+	'result-g': [{
+		name: '病竇症候群',
+		link: 'https://kb.commonhealth.com.tw/library/593.html',
+	},{
+		name: '房室傳導阻滯',
+		link: 'https://kb.commonhealth.com.tw/library/562.html',
+	}],
+	'result-h': [{
+		name: '心房纖維顫動心房撲動',
+		link: 'https://kb.commonhealth.com.tw/library/21.html',
+	}],
+	'result-i': [{
+		name: '竇性心搏過速',
+		link: 'https://kb.commonhealth.com.tw/library/593.html',
+	}],
+	'result-j': [{
+		name: '陣發性上心室心搏過速',
+		link: 'https://kb.commonhealth.com.tw/library/593.html',
+	}, {
+		name: '期外收縮',
+		link: 'https://kb.commonhealth.com.tw/library/559.html',
+	}],
+	'result-k': [{
+		name: '期外收縮',
+		link: 'https://kb.commonhealth.com.tw/library/559.html',
+	}],
+	'result-l': [{
+		name: '緊張、焦慮情緒疾病',
+		link: 'https://kb.commonhealth.com.tw/library/427.html',
+	}, {
+		name: '更年期引發的心悸',
+		link: 'https://kb.commonhealth.com.tw/library/64.html',
+	}],
+}
 
-				if (nextQuestion.length !== 0) {
-					$('html, body').animate({
-						scrollTop: nextQuestion.offset().top
-					}, 1000);
-				}
-
-				$(this).addClass("selected");
-				$(this).siblings('.selected').removeClass('selected');
-				checkanswer(answer);
-			});
-	}
-	function checkanswer(answer) {
-		userSelect[question] = answer;
-		question = question + 1;
-		count();
-		if (question < 6) {
-			disableScroll();
-		}
-		if (question == 6) {
-			final();
-			chartMake();
-			$("html, body").animate({
-				scrollTop: $('#resultSection').offset().top
-			}, 1000);
-			enableScroll();
-		}
-	}
-	function count() {
-		a1 = parseInt(userSelect[0]);
-		a2 = parseInt(userSelect[1]);
-		a3 = parseInt(userSelect[2]);
-		a4 = parseInt(userSelect[3]);
-		a5 = parseInt(userSelect[4]);
-		a6 = parseInt(userSelect[5]);
-	}
-	$(".reset").on("click", function () {
-		reset();
-		$("html, body").animate({ scrollTop: 0 }, 600);
-		return false;
+function insertQuiz() {
+	let string = ``;
+	quiz.forEach(item => {
+		let options = ``;
+		item.option.forEach(element => {
+			// console.log(element);
+			options += `<button type="button" class="test-btn" data-next="${element.next}"
+				data-result="${element.result}"
+				data-category="web_web" data-action="2022CVD" data-label="${element.track}
+			">${element.content}</button>`;
+		});
+		string += `<div class="container outer"><div class="row"><div class="inner" id="${item.id}">
+			<h3>${item.title}</h3>
+			${options}
+			<button type="button" class="pre-btn" data-prev="${item.prev}">回上一題</button>
+			</div></div></div>`;
 	});
-	$(".shared").on("click", function () {
-		return false;
+	$('#test .test-inner').html(string);
+}
+insertQuiz();
+
+function goResult(val) {
+	let resultDisease = resultArray[val];
+	let str = '';
+	resultDisease.forEach(element => {
+		str += `<div class="result-circle">
+			<a href="${element.link}" target="_blank">
+				<div class="disease-name">${element.name}</div>
+				<h6>認識疾病 →</h6>
+			</a>
+		</div>`;
 	});
-	function reset() {
-		a1 = 0;
-		a2 = 0;
-		a3 = 0;
-		a4 = 0;
-		a5 = 0;
-		a6 = 0;
-		question = 0;
-		userSelect = [0, 0, 0, 0, 0, 0];
-		$('.op-block').show();
-		$('.op-block > div').scrollTop(0);
+	$('#resultSection .disease-block').html(str);
+	$('#resultSection').show();
+	$([document.documentElement, document.body]).animate({
+		scrollTop: $('#resultSection').offset().top - 100
+	}, 500);
+	enableScroll();
+	$('body').removeClass('noscroll');
+}
+
+function backPrev(val) {
+	console.log(val);
+	let index = val.split('-q')[1];
+	let outerHeight = $('#test .outer').height();
+	$('#test .test-inner').animate({
+		scrollTop: (index-1) * outerHeight
+	}, 500);
+}
+
+function resetTest() {
+	$('#test .test-inner').animate({
+		scrollTop: 0 * outerHeight
+	}, 0);
+	$([document.documentElement, document.body]).animate({
+		scrollTop: $('#test').offset().top - headerOffset
+	}, 500);
+	$('#resultSection').hide();
+	disableScroll();
+	$('body').addClass('noscroll');
+}
+
+$('.navbar-nav li a').click(function () {
+	if ( $(this).attr('id') === 'playTest' ) {
+		disableScroll();
 		$('body').addClass('noscroll');
-		$("#resultSection, .result-block").hide();
-		$(".choice").siblings('.selected').removeClass('selected');
-		$(".choice").siblings('').attr('checked', false);
-		// choiceScore();
+	} else {
+		enableScroll();
+		$('body').removeClass('noscroll');
 	}
-	function final() {
-		$("#resultSection").show();
+})
+
+$('#test .test-btn').click(function() {
+	let prev = $(this).siblings('.pre-btn').attr('data-prev');
+	let next = $(this).attr('data-next');
+	let index = next.split('-q')[1];
+	let result = $(this).attr('data-result');
+	let outerHeight = $('#test .outer').height();
+	if ( next !== 'end' ) {
+		$('#test .test-inner').animate({
+			scrollTop: (index-1) * outerHeight
+		}, 500);
+	} else {
+		goResult(result);
 	}
-	function chartMake() {
-		var totalScore = a1 + a2 + a3 + a4 + a5 + a6;
-		if (a6 >= 2) {
-			$("#result4").show();
-		} else if ((totalScore >= 0) && (totalScore < 6)) {
-			$("#result1").show();
-		} else if ((totalScore >= 6) && (totalScore < 10)) {
-			$("#result2").show();
-		} else if ((totalScore >= 10) && (totalScore < 15)) {
-			$("#result3").show();
-		} else if (totalScore >= 15) {
-			$("#result4").show();
-		}
-	}
+});
+
+$('#test .pre-btn').click(function() {
+	let prev = $(this).attr('data-prev');
+	backPrev(prev);
+});
+
+$('#test-again').click(function() {
+	resetTest();
 });
